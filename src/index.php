150902a1f8606a38982dbf0922e250c824e282e8
bitoffluff1 <lena_khmelnikova@mail.ru>
2020-09-11 00:56:26 +0300

    Lesson 6

--- src/index.php
+++ src/index.php
@@ -1,22 +1,12 @@
 <?php
-$start = microtime(true);
+session_start();
 
-$redis = new Redis();
-$redis->connect('172.19.0.1', 6379);
+$memcached = new Memcached();
+$memcached->addServer('172.19.0.4', 11211);
 
-if ($redis->get('customerName')) {
-    var_dump(345);
-    echo $redis->get('customerName');
-} else {
-    var_dump(123);
-    $link = mysqli_connect('172.19.0.3:3306', 'root', 'qwerty', 'high_project');
-    $sql = "select * from customers where contactFirstName='Tony'";
-    $result = mysqli_query($link, $sql);
-    while ($row = mysqli_fetch_array($result)) {
-        $redis->set('customerName', $row['customerName']);
-        echo $row['customerName'];
-    }
-}
+echo '<pre>'; print_r($memcached->getServerList()); echo '</pre>';
 
-$time = microtime(true) - $start;
-echo $time;
+$_SESSION['favcolor'] = 'green';
+$_SESSION['animal']   = 'cat';
+
+var_dump($_SESSION);
